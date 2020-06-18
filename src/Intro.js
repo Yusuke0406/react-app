35bdf063413e4b9efedbc64820677ac6c3b964a7
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2020-06-19 01:19:27 +0900

    軽微な変更

--- src/Intro.js
+++ src/Intro.js
@@ -6,28 +6,28 @@ class Intro extends React.Component{
         <div class="black__container">
           <div class="black__container__comment1">
             <p class="black__container__comment1__p">はじめまして、原田祐輔と申します。</p>
-            <p class="black__container__comment1__p">愛知県豊川市出身の２２歳で</p>
+            <p class="black__container__comment1__p">愛知県豊川市出身の２3歳で</p>
             <p class="black__container__comment1__p">現在、関西外国語大学に在学中です。</p>
           </div>
           <div class="black__container__comment1">
-            <p class="black__container__comment1__p">大学では語学を学んでいます。</p>
+            <p class="black__container__comment1__p">大学では語学を専攻しており</p>
             <p class="black__container__comment1__p">中国、カナダ、フィリピンに</p>
             <p class="black__container__comment1__p">留学経験があります。</p>
           </div>
           <div class="black__container__comment1">
-            <p class="black__container__comment1__p">フィリピン留学時に</p>
-            <p class="black__container__comment1__p">プログラミングの授業があり</p>
-            <p class="black__container__comment1__p">そこでプログラミングに出会いました</p>
+            <p class="black__container__comment1__p">大学３年生で</p>
+            <p class="black__container__comment1__p">プログラミングに興味を持ち</p>
+            <p class="black__container__comment1__p">休学し、プログラミングを学ぶことを決心</p>
           </div>
           <div class="black__container__comment1">
-            <p class="black__container__comment1__p">４ヶ月間学校に通い</p>
-            <p class="black__container__comment1__p">プログラミングにハマりました</p>
-            <p class="black__container__comment1__p">帰国後、独学で学習を始めました。</p>
+            <p class="black__container__comment1__p">４ヶ月間、PHPを学ぶ</p>
+            <p class="black__container__comment1__p">完全にプログラミングにハマりました。</p>
+            <p class="black__container__comment1__p">帰国後、もっと知識を深めたくて</p>
           </div>
           <div class="black__container__comment1">
-            <p class="black__container__comment1__p">その後、techexpertに入学しました。</p>
-            <p class="black__container__comment1__p">チーム開発などを経験し</p>
-            <p class="black__container__comment1__p">無事卒業しました。</p>
+            <p class="black__container__comment1__p">TechExpertへ</p>
+            <p class="black__container__comment1__p">知識を深め、無事卒業</p>
+            <p class="black__container__comment1__p">現在、就職活動中です。</p>
           </div>
         </div>
       </div>
